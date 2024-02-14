a804444444a48558742f31c15761e5ec4eba7648
ankush25 <ankushthummar25@gmail.com>
2024-02-14 09:58:43 +0530

    14-2-2024

---
+++ Example/basic.js
@@ -0,0 +1,44 @@
+// output in JS
+
+// console.log("Skill Qode")
+// alert('Hi i am learning JAVASCRIPT');
+// document.write("Hi I Am Learning JAVASCRIPT")
+
+// HTML DOM METhod
+// document.getElementById("a").innerHTML = 'Hello , i am Ankush Thummar'
+
+// console.log(1 ^ 1)
+// console.log(0 ^ 0)
+// console.log(1 ^ 0)
+// console.log(0 ^ 1)
+
+// let a = ""; // -> undefines  {}
+// console.log(a)
+
+// console.log(10 + 20)
+// console.log("10" - 20)
+// console.log("Skill" + 20)
+// console.log("2" * 20)
+// console.log(typeof("SKill / 20"))
+
+
+// let x = Number(prompt('Enter x value : '));     // by defualt -> String
+// console.log((x));
+
+// loop -> For While dowhile
+// let t = "";
+// for(let i = 1 ; i < 10 ; i++){
+//     t = t + i + "\t";
+// }
+// console.log(t);
+
+
+
+let a=13 , t = "";
+do{
+    t = t + a + "\t";
+    a++;
+}while(a<=15);
+console.log(t);
+
+
